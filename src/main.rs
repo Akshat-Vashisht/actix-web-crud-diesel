ac4f721600d11913562cf3df57572c5a81807ee1
Akshat-Vashisht <akashat2003@gmail.com>
2023-11-10 17:43:06 +0530

    Updated Schema

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 use actix_web::{web, App, HttpServer, Responder};
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use dotenvy::dotenv;
+use std::env;
 
 mod routes;
+mod models;
+mod schema;
+
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is missing");
+    PgConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
 
 
 #[actix_web::main]
